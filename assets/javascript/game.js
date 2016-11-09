d1ca1d2901494d453444a26639aed2343b05821c
ttruong89 <t.truong2009@gmail.com>
2016-11-08 20:10:06 -0600

    updated the js for more functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,13 +5,11 @@ var wordArray = ["Turkey", "Gravy", "Pilgrims", "Thankful", "Giblets", "Mashed P
 
 var playerLetter = "";
 
-var computerChoice = wordArray[(Math.floor(Math.random()*(wordArray.length-1)))]; 
+var computerChoice;
 
 var guessesRemaining = 8;
 
-var guesses;
-
-var incorrectGuesses;
+var guesses = [];
 
 var wins = 0;
 
@@ -20,50 +18,71 @@ var losses = 0;
 
 // Will add on to score.  Will reset guessesRemaining back to 8.
 
-function gameReset(win){
-    document.getElementbyID("#wins");
-    wins++;
-    guessesRemaining = 8;
-    guesses;
-};
 
-function gameReset(lose){
-	document.getElementbyID("losses");
-	losses++;
-	guessesRemaining = 8;
-};
 
+    	//var underscores = "";
+		//for (i=0; i<computerChoice.length; i++){
+		//underscores = underscores + "_ ";
+
+
+function gameReset(playerWins){
+		guessesRemaining = 8;
+    	guesses = [];
+    	computerChoice = wordArray[(Math.floor(Math.random()*(wordArray.length+1)))];
+    	document.getElementById("guessesRemaining").innerHTML = guessesRemaining; 
+
+    if (playerWins === "win"){
+		wins++;  
+		document.getElementById("#wins");	 		
+    }
+    else { //lose scenario
+    	losses++;
+    	document.getElementById("losses");
+    	//rolling the computerChoice dice.
+    }
+};
 
 // Event listener for "press any key" to play that triggers once clicked.
-document.onkeyup = function(event) {
-	return String.fromCharCode(event.keyCode);
-}
+// As player clicks letters, guessesRemaining will countdown.
+
+document.onkeyup = function(event) { //gamereset if guessesRemaining <= 0
+		var playerLetter = String.fromCharCode(event.keyCode).toUpperCase();
+
+		if (event.keyCode <= 65 || event.keyCode >= 90){
+			alert("Please pick a letter!");
+			return;
+		}
+
+		if (guesses.indexOf(playerLetter) > -1) {
+			return;
+		}
+
+		guessesRemaining--;
+		document.getElementById("guessesRemaining").innerHTML = guessesRemaining;
+		guesses.push(playerLetter);
+		document.getElementById("incorrectGuesses").innerHTML = guesses;
+
+		if (guessesRemaining <= 0) {
+			//playerlose
+			gameReset();
+			alert("You Lose!");
+		}
+		else {
+			//get the word correct.
+		}
+
+
+		document.getElementById("guessBlanks").innerHTML = playerLetter;
+};
+
 
 // This will happen again once the game is reset.  
 
-// Generate a random word from the wordArray.
-function randomWord(){
-	return computerChoice;
-};
-
 // Line for any random word picked by computer will appear.
-
-// Player clicks on key for letter guesses.  
-document.onkeyup = function (event) {
-	var playerLetter = String.fromCharCode(keystroke.keyCode).toUpperCase();
-}
-// Require that the key is alpha.  An alert popup saying, only letters can be used.
-	
-
-// As player clicks letters, guessesRemaining will countdown.  
-
-// Letters of the randomWord will appear as they are correct.
-
-// If player runs out of guessesRemaning, alert will pop up, "you lose".
 
 // If word is correct, tally win by +1 increment.
 
-// If word is incorrect, tally loss by -1 increment.
+// If word is incorrect, tally loss by +1 increment.
 
 // Change text on webpage allowing player to press any key to start new game.
 
